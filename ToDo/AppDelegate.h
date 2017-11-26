fc71efd07423bd376bc5fb5b04fffe77a5a7f328
Will Tachau <wtachau@gmail.com>
2017-11-26 12:20:07 -0800

    Initial Commit

---
+++ ToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by William Tachau on 11/26/17.
+//  Copyright © 2017 Tachau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDo
+//
+//  Created by William Tachau on 11/26/17.
+//  Copyright © 2017 Tachau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
